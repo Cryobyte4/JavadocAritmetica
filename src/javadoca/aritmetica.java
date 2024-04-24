fe2d558aa0988a77b37cc99b36d49d4ec39298e5
Cryobyte4 <david09rubio@hotmail.com>
2024-04-24 20:40:30 +0200

    Se añade la documentación

--- src/javadoca/aritmetica.java
+++ src/javadoca/aritmetica.java
@@ -8,20 +8,47 @@ package javadoca;
  */
 public class aritmetica {
 
-
+    /**
+     * Suma dos números enteros.
+     * 
+     * @param a el primer número
+     * @param b el segundo número
+     * @return la suma de a y b
+     */
     public int sumar(int a, int b) {
         return a + b;
     }
 
-
+    /**
+     * Resta dos números enteros.
+     * 
+     * @param a el primer número
+     * @param b el segundo número
+     * @return la resta de a menos b
+     */
     public int restar(int a, int b) {
         return a - b;
     }
 
+    /**
+     * Multiplica dos números enteros.
+     * 
+     * @param a el primer número
+     * @param b el segundo número
+     * @return el producto de a por b
+     */
     public int multiplicar(int a, int b) {
         return a * b;
     }
-
+    
+    /**
+     * Divide dos números enteros.
+     * 
+     * @param a el dividendo
+     * @param b el divisor
+     * @return el resultado de la división de a entre b
+     * @throws IllegalArgumentException si b es cero
+     */
     public double dividir(int a, int b) {
         if (b == 0) {
             throw new IllegalArgumentException("No se puede dividir por cero");
--- src/javadoca/operaciones.java
+++ src/javadoca/operaciones.java
@@ -8,6 +8,14 @@ package javadoca;
  */
 public class operaciones {
 
+    /**
+     * Calcula el módulo de dos números enteros.
+     * 
+     * @param a el dividendo
+     * @param b el divisor
+     * @return el resto de la división de a entre b
+     * @throws IllegalArgumentException si el divisor es cero
+     */
     public int modulo(int a, int b) {
         if (b == 0) {
             throw new IllegalArgumentException("El divisor no puede ser cero");
@@ -15,16 +23,28 @@ public class operaciones {
         return a % b;
     }
 
- 
+     /**
+     * Calcula el cuadrado de un número entero.
+     * 
+     * @param a el número a elevar al cuadrado
+     * @return el cuadrado de a
+     */
     public int calcularCuadrado(int a) {
         return a * a;
     }
-
+    
+    /**
+     * Calcula la raíz cuadrada de un número.
+     * 
+     * @param a el número del cual se calculará la raíz cuadrada
+     * @return la raíz cuadrada de a
+     * @throws IllegalArgumentException si a es un número negativo
+     */
     public double calcularRaizCuadrada(double a) {
         if (a < 0) {
             throw new IllegalArgumentException("No se puede calcular la raíz cuadrada de un número negativo");
         }
         return Math.sqrt(a);
-    }
+    }   
 }
 
